d019a5ded838c7445fd337ae98cd46dc71dc2fc3
Thyago da Silva <chmod7000@pm.me>
2020-08-08 22:22:01 -0300

    refactor

--- components/productRow.js
+++ components/productRow.js
@@ -6,6 +6,7 @@ import {
     CaretDownIcon,
     IconButton,
     Heading,
+    minorScale,
     majorScale,
     MinusIcon,
     PlusIcon,
@@ -23,35 +24,34 @@ const $ = require("sanctuary-def")
 const ProductRow = ({ product, viewingCart, userId }) => {
     // TODO: quantity needs to be in state  so it changes on updates
     // we get the cart updated from the api call so it should set the state
-    // from there and maybe pass it down the line. or set the cart in context.
+    // from there and maybe pass it down the line. 
 
     // // Array of objects predicate
     const arrayOfObjects = S.is($.Array($.Object))
 
     const variants = S.get(arrayOfObjects)("variants")(product)
     const [flatVariants, setVariantList] = useState(S.fromMaybe([])(variants))
-    // const flatVariants = S.fromMaybe([])(variants)
 
     const mbFirstVariantsId = S.pipe([
         S.chain(S.head),
         S.chain(S.get(S.is($.String))("_id"))
     ])(variants)
-    
-    const mbSelectedVariantId = S.maybeToNullable(mbFirstVariantsId)
-    
-    const [selectedVariant, setSelectedVariant] = useState(mbSelectedVariantId)
-    
-    const mbVariantObject = findByIdInList(selectedVariant)(flatVariants)
-    const selected = S.fromMaybe ({}) (mbVariantObject)
 
-    // const mbSourceId = S.map(S.prop("source"))(mbVariantObject)
-    // const mbSource = findByIdInList(S.fromMaybe("não encontrado")(mbSourceId))(sources)
+    const mbSelectedVariantId = S.maybeToNullable(mbFirstVariantsId)
+
+    const [selectedVariant, setSelectedVariant] = useState(mbSelectedVariantId)
+
+    const mbVariantObject = findByIdInList(selectedVariant)(flatVariants)
+
+    if (S.isNothing(mbVariantObject)) return <Alert intent="danger" title="Aqui jaz um produto malcriado." />
+
+    const selected = S.fromMaybe({})(mbVariantObject)
 
     const q = S.fromMaybe(0)(S.get(S.is($.Number))("quantity")(selected))
-    const [ quantity, setQuantity ] = useState (q)
+    const [quantity, setQuantity] = useState(q)
 
     const nextCartState = async (delta) => {
-        setQuantity (S.max (0) (quantity + delta))
+        setQuantity(S.max(0)(quantity + delta))
         const res = await fetch("http://localhost:3000/api/cart", {
             method: "post",
             body: JSON.stringify({
@@ -64,64 +64,58 @@ const ProductRow = ({ product, viewingCart, userId }) => {
         console.log("nextCart", res)
     }
 
-    // short-circuit if no valid variant is found
-    return ((S.isNothing(mbVariantObject)))
-        ? (<Alert
-            intent="danger"
-            title="Aqui jaz um produto malcriado."
-        />)
-        : (
-            <Table.Row display="flex" key={id(product)} height="auto" padding={majorScale(1)} flexWrap="wrap" backgroundColor={"white"}>
+    return (
+        <Table.Row display="flex" key={id(product)} height="auto" padding={minorScale (1)} flexWrap="wrap" backgroundColor={"white"}>
 
-                {viewingCart && <Table.TextCell flexBasis={60} flexGrow={0} flexShrink={0}>
-                    <Badge color="red">{ quantity }</Badge>
-                </Table.TextCell>}
+            {(viewingCart) && <Table.TextCell flexBasis={60} flexGrow={0} flexShrink={0}>
+                <Badge color="red">{quantity}</Badge>
+            </Table.TextCell>}
 
-                <Table.TextCell flexBasis={380} paddingY={majorScale(1)}>
-                    <Heading size={500} whiteSpace="normal">{S.prop("label")(product)}</Heading>
-                </Table.TextCell>
-                <Table.TextCell display="flex" flexBasis={380} paddingY={majorScale(1)}>
-                    {!viewingCart ? (
-                        <Popover
-                            content={({ close }) => (
-                                <Table>
-                                    <Table.Head>
-                                        <Table.TextCell>
-                                            Escolha o melhor
+            <Table.TextCell flexBasis={380} paddingY={majorScale(1)}>
+                <Heading size={500} whiteSpace="normal">{S.prop("label")(product)}</Heading>
+            </Table.TextCell>
+            <Table.TextCell display="flex" flexBasis={380} paddingY={majorScale(1)}>
+                {!viewingCart ? (
+                    <Popover
+                        content={({ close }) => (
+                            <Table>
+                                <Table.Head>
+                                    <Table.TextCell>
+                                        Escolha o melhor
                                         </Table.TextCell>
-                                    </Table.Head>
-                                    <Table.Body>
-                                        {S.map(v =>
-                                            <Table.Row key={id(v)} isSelectable onSelect={() => {
-                                                close()
-                                                setSelectedVariant(id(v))
-                                            }}>
-                                                <Variant variant={v} />
-                                            </Table.Row>
-                                        )(flatVariants)}
-                                    </Table.Body>
-                                </Table>
-                            )}>
-                            <Button iconAfter={CaretDownIcon}>
-                                <Variant variant={selected} />
-                            </Button>
-                        </Popover>)
-                        : (<Variant variant={selected} />)
-                    }
-                </Table.TextCell>
-                <Table.TextCell flexBasis={150} paddingY={majorScale(1)}>
-                    <Pane
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="space-between">
-                        <Button flex={"1"} appearance="minimal" onClick={() => nextCartState(-Infinity)}
-                            intent="danger">remover</Button>
-                        <IconButton flex={"1"} onClick={() => nextCartState(-1)} icon={MinusIcon} />
-                        <IconButton flex={"1"} onClick={() => nextCartState(1)} icon={PlusIcon} />
-                    </Pane>
-                </Table.TextCell>
-            </Table.Row>
-        )
+                                </Table.Head>
+                                <Table.Body>
+                                    {S.map(v =>
+                                        <Table.Row key={id(v)} isSelectable onSelect={() => {
+                                            close()
+                                            setSelectedVariant(id(v))
+                                        }}>
+                                            <Variant variant={v} />
+                                        </Table.Row>
+                                    )(flatVariants)}
+                                </Table.Body>
+                            </Table>
+                        )}>
+                        <Button iconAfter={CaretDownIcon}>
+                            <Variant variant={selected} />
+                        </Button>
+                    </Popover>)
+                    : (<Variant variant={selected} />)
+                }
+            </Table.TextCell>
+            <Table.TextCell flexBasis={150} paddingY={majorScale(1)}>
+                <Pane
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between">
+                    <Button flex={"1"} appearance="minimal" onClick={() => nextCartState(-Infinity)}
+                        intent="danger">remover</Button>
+                    <IconButton flex={"1"} onClick={() => nextCartState(-1)} icon={MinusIcon} />
+                    <IconButton flex={"1"} onClick={() => nextCartState(1)} icon={PlusIcon} />
+                </Pane>
+            </Table.TextCell>
+        </Table.Row>
+    )
 }
 
 export default ProductRow
