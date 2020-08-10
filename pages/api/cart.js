2709d541bfb0025826ecfc17d9523c1b3cc7d138
Thyago da Silva <chmod7000@pm.me>
2020-08-10 04:02:02 -0300

    refactor cart api

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -16,25 +16,36 @@ const initialCart = {
 
 const getLatestCart = async carts => await carts.find({}, { sort: { "_id": -1 }, limit: 1 }).toArray()
 
+const grabCartsProducts = async (latestCart, productsCollection) => {
+  debugger
+  const productIds = S.unchecked.keys(S.prop("items")(latestCart))
+  const productObjectIds = S.map(ObjectId)(productIds)
+
+  const query = { "_id": { $in: productObjectIds } }
+  const ps = await productsCollection.find(query).toArray()
+  return ps
+}
+
 handler.use(middleware)
 
 handler.get(async (req, res) => {
-  
+  debugger
   // TODO: Filter by owner
-  const carts = await req.db.collection('carts')
+  const carts = await req.db.collection("carts")
   
   const cart = await getLatestCart (carts)
 
-  
   // get latest cart or seed a new one, TODO: filter by user
   // TODO: get "simples" flag here and pick appropriate price
   const latestCart = S.fromMaybe ({...initialCart}) (S.head (cart))
 
-  const productIds = S.unchecked.keys (S.prop ("items") (latestCart))
-  const productObjectIds = S.map (ObjectId) (productIds)
+  // const productIds = S.unchecked.keys (S.prop ("items") (latestCart))
+  // const productObjectIds = S.map (ObjectId) (productIds)
 
-  const query = { "_id": { $in: productObjectIds } }
-  const products = await req.db.collection('products').find(query).toArray()
+  const productsCollection = await req.db.collection("products")
+  // const query = { "_id": { $in: productObjectIds } }
+  // const products = await req.db.collection('products').find(query).toArray()
+  const products = await grabCartsProducts (latestCart, productsCollection)
 
   // TODO: only actually add the products if we were asked with a 
   // ?with-products
@@ -59,7 +70,6 @@ handler.get(async (req, res) => {
 
   res.status(200).json({ cart: latestCart, products: productsWithQuantities, totalPrice, itemsInCart })
 });
-
 
 
 handler.post(async (req, res) => {
