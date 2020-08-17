5d9ed74d65e452520ef16c6f0b06b9ff137357cc
Thyago da Silva <chmod7000@pm.me>
2020-08-16 21:57:59 -0300

    Changes baseurl to prod

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production"
 
-export const server = dev ? "http://localhost:3000" : "https://thirdprima.vercel.app"
+export const server = dev ? "http://localhost:3000" : "https://prima.market"
