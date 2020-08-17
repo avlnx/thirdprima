b3c715ec02bf5a78f4b6fb5c3af84e21fb9f233e
Thyago da Silva <chmod7000@pm.me>
2020-08-17 00:42:55 -0300

    Removes full height from spinnerbox

--- components/spinnerBox.js
+++ components/spinnerBox.js
@@ -3,7 +3,8 @@ import { brand } from "../theme"
 
 const SpinnerBox = ({ backgroundColor }) => {
   return (
-    <Pane height="100vh" background={backgroundColor || brand} display="flex" flex="1" alignItems="center" justifyContent="center" >
+    <Pane background={backgroundColor || brand} display="flex" flex="1" alignItems="center" justifyContent="center" >
+
       <Spinner size={64} />
     </Pane>
   )
