0155faa92aace44b5f5df73d081c32980c92437e
Thyago da Silva <chmod7000@pm.me>
2020-08-17 00:55:33 -0300

    Better loading

--- components/spinnerBox.js
+++ components/spinnerBox.js
@@ -4,8 +4,9 @@ import { brand } from "../theme"
 const SpinnerBox = ({ backgroundColor }) => {
   return (
     <Pane background={backgroundColor || brand} display="flex" flex="1" alignItems="center" justifyContent="center" >
-
-      <Spinner size={64} />
+      <Pane height={64} margin={0} padding={0}>
+        <Spinner size={64} />
+      </Pane>
     </Pane>
   )
 }
