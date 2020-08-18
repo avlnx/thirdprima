7d7df669810d82c00c2814f6faa776073ab7685e
Thyago da Silva <chmod7000@pm.me>
2020-08-18 19:32:44 -0300

    catch email error

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -69,8 +69,11 @@ export default async (req, res) => {
           // success, insert seed cart to reset and send message
           const result2 = await carts.insertOne(userInitialCart(S.prop ("owner") (cart)))
           // notify purchase but don't wait for promisse
-          const emailResponse = await notifyPurchase (cart)
-          console.log("emailResponse", emailResponse)
+          try{
+            const emailResponse = await notifyPurchase (cart)
+          } catch (err) {
+            console.error(err)
+          }
           if (S.prop("insertedCount")(result2) === 1) {
             return res.status(200).json({ success: "Tudo certo. Obrigada pela sua compra. Vamos preparar seu pedido e entraremos em contato em breve." })
           } else {
