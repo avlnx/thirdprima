698cf649da4086e44a8ad9b878ad9edb0b74bc1d
Thyago da Silva <chmod7000@pm.me>
2020-09-06 06:03:16 -0300

    select variants interface in master page

--- components/cardContent.js
+++ components/cardContent.js
@@ -1,12 +1,14 @@
 import { Card, majorScale, Text } from "evergreen-ui"
+import {primaTheme} from "../theme"
 
-const CardContent = ({ key, title, children }) => {
+const S = require("sanctuary")
+
+const CardContent = ({ title, selected, onClick, v, children }) => {
+  const darkBg = S.props(["palette", "neutral", "base"])(primaTheme)
   return (
-    <Card flexBasis={"27%"} marginRight={"2%"} marginBottom={"2%"} flexGrow={1} padding={majorScale(2)} elevation={1} width={300} display="flex" flexDirection="column" justifyContent="space-between" key={key} background={"white"} onClick={() => alert("oi")}>
-      <Text style={{ "margin": 0, "marginBottom": "10px" }}>{title}</Text>
-      <small><pre style={{ "margin": 0 }}>
-        {children}
-      </pre></small>
+    <Card flexBasis={"27%"} marginRight={"2%"} marginBottom={"2%"} flexGrow={1} padding={majorScale(2)} elevation={1} width={300} display="flex" flexDirection="column" justifyContent="space-between" background={selected ? darkBg : "white"} onClick={() => onClick(v)} >
+      <Text style={{ "margin": 0, "marginBottom": "10px", "color": selected ? "white" : darkBg }}>{title}</Text>
+      {children}
     </Card>
   )
 }
--- pages/master.js
+++ pages/master.js
@@ -1,21 +1,38 @@
 
-import { Card, Pane } from "evergreen-ui"
+import { Badge, Button, Card, IconButton, majorScale, Pane, PlusIcon,  Text } from "evergreen-ui"
 import Head from "next/head"
 import { primaTheme } from "../theme"
 import connect from "../lib/db"
-import { Button, majorScale, minorScale, Text } from "evergreen-ui"
 import CardContent from "../components/cardContent"
+import { useState } from "react"
 
 const S = require("sanctuary")
 const $ = require("sanctuary-def")
 
-const Master = ({variants, sources}) => {
-  console.log("variants", variants)
+const Master = ({ variants, sources }) => {
   const lightBg = S.props(["palette", "neutral", "light"])(primaTheme)
-  const getSource = id => S.maybeToNullable (S.find (s => S.prop ("_id") (s) == (id)) (sources))
+  const getSource = id => S.maybeToNullable (S.find (source => S.prop ("_id") (source) == (id)) (sources))
+  const [ selectedVs, setSelectedVs ] = useState([])
 
+  const filteredVs = v => S.filter(variant => { return (variant._id != v._id) })
 
-  
+  const selectVariant = v => {
+    // const filteredVs = S.filter(variant => { return (variant._id != v._id) })(selectedVs)
+    const filtered = filteredVs (v) (selectedVs)
+    setSelectedVs([...filtered, v])
+  }
+  const boundSelVar = selectVariant.bind(this)
+
+  const deselectVariant = v => {
+    const filtered = filteredVs (v) (selectedVs)
+    setSelectedVs(filtered)
+  }
+  const boundDeselVar = deselectVariant.bind(this)
+
+  const isSelected = v => S.size(S.filter(variant => { return (variant._id == v._id) })(selectedVs)) > 0
+
+  const availableVs = S.unchecked.reject (isSelected) (variants)
+
   return (
     <>
       <Head>
@@ -28,17 +45,39 @@ const Master = ({variants, sources}) => {
           <p>Oi Admin. Clique nas variações que você deseja agrupar em um produto. Depois é só clicar no botão e escolher (ou criar) o produto.</p>
         </Pane>
 
+        <Pane display="flex" flexWrap="wrap">
+          {S.size(selectedVs) == 0 && <Text>As variações que você selecionar vão aparecer aqui.</Text>}
+          {S.unchecked.map(v => (
+            <CardContent key={v._id} title={v.label} selected={true} onClick={boundDeselVar} v={v}>
+              <Pane display="flex" justifyContent="space-between">
+                <small>
+                  <pre style={{ "margin": 0, "color": "#ccc" }}>
+                    R$ {v.price} / {v.pack_size}{v.pack_unit}
+                  </pre>
+                </small>
+                <Badge color="purple" marginRight={8}>@{getSource(v.source).symbol}</Badge>
+              </Pane>
+            </CardContent>
+          ))(selectedVs)}
+        </Pane>
+
         <Pane paddingY={majorScale(2)}>
           <Button appearance="primary">Escolher produto para as variações selecionadas</Button>
         </Pane>
 
         <Pane display="flex" flexWrap="wrap">
           {S.unchecked.map(v => (
-            <CardContent key={v._id} title={v.label}>
-              @{getSource(v.source).label}<br />
-                R$ {v.price} / {v.pack_size}{v.pack_unit}
+            <CardContent key={v._id} title={v.label} selected={false} onClick={boundSelVar} v={v}>
+              <Pane display="flex" justifyContent="space-between">
+                <small>
+                  <pre style={{ "margin": 0 }}>
+                    R$ {v.price} / {v.pack_size}{v.pack_unit}
+                  </pre>
+                </small>
+                <Badge color="purple" marginRight={8}>@{getSource(v.source).symbol}</Badge>
+              </Pane>
             </CardContent>
-          ))(variants)}
+          ))(availableVs)}
         </Pane>
 
       </Pane>
